5827ce652981363f0d66ee29c308ff4083d50427
TITIBEBE <68087115+AssembleCat@users.noreply.github.com>
2023-11-17 02:06:40 +0900

    [feat] HttpRequester POST 요청 응답을 지정할 수 있도록 수정 및 ResponseBody 받는 공통로직을 분리

--- src/main/java/service/http/HttpRequester.java
+++ src/main/java/service/http/HttpRequester.java
@@ -1,15 +1,20 @@
 package service.http;
 
 import com.google.gson.Gson;
+
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class HttpRequester {
-    public <T> T sendGetRequest(String urlString, Type typeOfT) {
+    Gson gson = new Gson();
+
+    public <T> T sendGetRequest(String urlString, Type responseType) {
         try {
             URL url = new URL(urlString);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -17,17 +22,7 @@ public class HttpRequester {
 
             int responseCode = conn.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-
-                Gson gson = new Gson();
-                return gson.fromJson(response.toString(), typeOfT);
+                return getResponseBody(responseType, conn);
             } else {
                 System.out.println("GET request not worked");
                 return null;
@@ -38,7 +33,7 @@ public class HttpRequester {
         }
     }
 
-    public <T> T sendPostRequest(String urlString, Object requestData, Type typeOfT) {
+    public <T> T sendPostRequest(String urlString, Object requestData, Type responseType) {
         try {
             URL url = new URL(urlString);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -48,7 +43,6 @@ public class HttpRequester {
             conn.setRequestProperty("Accept", "application/json");
             conn.setDoOutput(true);
 
-            Gson gson = new Gson();
             String jsonInputString = gson.toJson(requestData);
 
             OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream(), StandardCharsets.UTF_8);
@@ -57,16 +51,7 @@ public class HttpRequester {
 
             int responseCode = conn.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-
-                return gson.fromJson(response.toString(), typeOfT);
+                return getResponseBody(responseType, conn);
             } else {
                 System.out.println("POST request not worked");
                 return null;
@@ -75,5 +60,18 @@ public class HttpRequester {
             System.out.println("Error: " + e.getMessage());
             return null;
         }
+    }
+
+    private <T> T getResponseBody(Type responseType, HttpURLConnection conn) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return gson.fromJson(response.toString(), responseType);
     }
 }
